8885d517501b6c076a18431975e3737d1c3c20d8
Y.Yamashiro <yamashiro.yoshihiro@mono.cafe>
2024-07-31 22:54:37 +0900

    タイマを使用するコードを追加

--- src/lib/mrubyc/hal.c
+++ src/lib/mrubyc/hal.c
@@ -13,28 +13,38 @@ static uint8_t memory_pool[MEMORY_SIZE];
 
 /* ===== prototype ===== */
 static int mrubyc_halinit(void);
-static int mrubyc_halmain(void);
 void mrubyc_haltick(struct k_work *const work);
 
 /* ===== kernel ===== */
 SYS_INIT(mrubyc_halinit, APPLICATION, 0);
 K_WORK_DEFINE(mrubyc_work, mrubyc_haltick);
-K_THREAD_DEFINE(mrubyc_thread, 384, mrubyc_halmain, NULL, NULL, NULL, -2,
-                K_ESSENTIAL, 0);
 
-/* ===== mrubyc_halinit ===== */
-static int mrubyc_halinit(void) {
-  mrbc_init(memory_pool, MEMORY_SIZE);
-  return EXIT_SUCCESS;
+#if !defined(MRBC_NO_TIMER)
+/* ===== use timer ===== */
+static void mrubyc_timerhandler(struct k_timer *const timer) {
+  k_work_submit(&mrubyc_work);
 }
-
-/* ===== mrubyc_halmain ===== */
+K_TIMER_DEFINE(mrubyc_timer, mrubyc_timerhandler, NULL);
+#else
+/* ===== MRBC_NO_TIMER ===== */
 static int mrubyc_halmain(void) {
   while (1) {
     k_work_submit(&mrubyc_work);
     k_msleep(1);
   }
   return EXIT_FAILURE;
+}
+K_THREAD_DEFINE(mrubyc_thread, 384, mrubyc_halmain, NULL, NULL, NULL, -2,
+                K_ESSENTIAL, 0);
+#endif
+
+/* ===== mrubyc_halinit ===== */
+static int mrubyc_halinit(void) {
+  mrbc_init(memory_pool, MEMORY_SIZE);
+#if !defined(MRBC_NO_TIMER)
+  k_timer_start(&mrubyc_timer, K_NO_WAIT, K_MSEC(1));
+#endif
+  return EXIT_SUCCESS;
 }
 
 /* ===== mrubyc_haltick ===== */
