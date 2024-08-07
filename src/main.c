3ec6b988688ca9ea7268d1e9affd3ee69f3e7aa0
Y.Yamashiro <yamashiro.yoshihiro@mono.cafe>
2024-08-08 00:13:49 +0900

    作業途中のコミット

--- src/main.c
+++ src/main.c
@@ -24,16 +24,20 @@
 #define LED0_NODE DT_ALIAS(led0)
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
+tdefl_compressor g_deflator;
+
 /* ===== prototype ===== */
 extern const uint8_t sample1[];
 extern const uint8_t sample2[];
 static void c_sw1_read(mrb_vm *vm, mrb_value *v, int argc);
 static void c_led1_write(mrb_vm *vm, mrb_value *v, int argc);
+static int bytecode_compress(void);
+static int bytecode_decompress(void);
 
 /* ===== main ===== */
 int main(void) {
   int ret;
-
+  bytecode_compress();
   if (!gpio_is_ready_dt(&led)) {
     return 0;
   }
@@ -62,3 +66,16 @@ static void c_sw1_read(mrb_vm *vm, mrb_value *v, int argc) {
 static void c_led1_write(mrb_vm *vm, mrb_value *v, int argc) {
   gpio_pin_set_dt(&led, GET_INT_ARG(1));
 }
+
+/* ===== bytecode_compress ===== */
+static int bytecode_compress(void) {
+  tdefl_status status;
+  const mz_uint kCompFlags =
+      TDEFL_WRITE_ZLIB_HEADER | 1500 | TDEFL_GREEDY_PARSING_FLAG;
+  if (TDEFL_STATUS_OKAY != tdefl_init(&g_deflator, NULL, NULL, kCompFlags)) {
+    return EXIT_FAILURE;
+  }
+}
+
+/* ===== bytecode_decompress ===== */
+static int bytecode_decompress(void);
